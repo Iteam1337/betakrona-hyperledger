8d3620ce8bcb8168836b82600f3813026610a679
Einar Persson <me@einarpersson.com>
2017-10-12 10:55:30 +0200

    Cleaned up test file

--- test/test.js
+++ test/test.js
@@ -27,28 +27,112 @@ const NS = 'org.riksbanken.ekrona';
 
 describe('e-krona', () => {
 
-    // let adminConnection;
+
     let businessNetworkConnection;
+    let factory
+    let events
+
+    let aliceIdentity
+    const aliceId ='1212121212'
+    let bobIdentity
+    const bobId = '1111111111'
+
+
+    /**
+     * Reconnect using a different identity.
+     * @param {Object} identity The identity to use.
+     * @return {Promise} A promise that will be resolved when complete.
+     */
+    function useIdentity(identity) {
+        return businessNetworkConnection.disconnect()
+            .then(() => {
+                businessNetworkConnection = new BusinessNetworkConnection({ fs: bfs_fs });
+                events = [];
+                businessNetworkConnection.on('event', (event) => {
+                    events.push(event);
+                });
+                return businessNetworkConnection.connect('defaultProfile', 'ekrona-network', identity.userID, identity.userSecret);
+            });
+    }
 
     before(() => {
+        // Initialize an in-memory file system, so we do not write any files to the actual file system.
         BrowserFS.initialize(new BrowserFS.FileSystem.InMemory());
+
+        // Create a new admin connection.
         const adminConnection = new AdminConnection({ fs: bfs_fs });
-        return adminConnection.createProfile('defaultProfile', {
-            type: 'embedded'
-        })
+
+        // Create a new connection profile that uses the embedded (in-memory) runtime.
+        return adminConnection.createProfile('defaultProfile', { type: 'embedded' })
             .then(() => {
+
+                // Establish an admin connection. The user ID must be admin. The user secret is
+                // ignored, but only when the tests are executed using the embedded (in-memory)
+                // runtime.
                 return adminConnection.connect('defaultProfile', 'admin', 'adminpw');
             })
             .then(() => {
+                // Generate a business network definition from the project directory.
                 return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
             })
             .then((businessNetworkDefinition) => {
+                // Deploy and start the business network defined by the business network definition.
                 return adminConnection.deploy(businessNetworkDefinition);
             })
             .then(() => {
+                // Create and establish a business network connection
                 businessNetworkConnection = new BusinessNetworkConnection({ fs: bfs_fs });
+                events = [];
+                businessNetworkConnection.on('event', (event) => {
+                    events.push(event);
+                });
                 return businessNetworkConnection.connect('defaultProfile', 'ekrona-network', 'admin', 'adminpw');
-            });
+            })
+            .then(() => {
+
+                // Get the factory for the business network.
+                factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+                // Create the participants.
+                const alice = factory.newResource(NS, 'Person', aliceId);
+                alice.firstName = 'Alice';
+                alice.lastName = 'A';
+                const bob = factory.newResource(NS, 'Person', bobId);
+                bob.firstName = 'Bob';
+                bob.lastName = 'B';
+                return businessNetworkConnection.getParticipantRegistry(`${NS}.Person`)
+                    .then((participantRegistry) => {
+                        participantRegistry.addAll([alice, bob]);
+                    });
+
+            })
+            .then(() => {
+
+                // Create the assets.
+                const asset1 = factory.newResource(NS, 'Account', '1');
+                asset1.owner = factory.newRelationship(NS, 'Person', '1212121212');
+                asset1.value = 100;
+                const asset2 = factory.newResource(NS, 'Account', '2');
+                asset2.owner = factory.newRelationship(NS, 'Person', '1111111111');
+                asset2.value = 200;
+                return businessNetworkConnection.getAssetRegistry(`${NS}.Account`)
+                    .then((assetRegistry) => {
+                        assetRegistry.addAll([asset1, asset2]);
+                    });
+            })
+            .then(() => {
+
+                // Issue the identities.
+                return businessNetworkConnection.issueIdentity(`${NS}.Person#${aliceId}`, 'alice')
+                    .then((identity) => {
+                        aliceIdentity = identity;
+                        return businessNetworkConnection.issueIdentity(`${NS}.Person#${bobId}`, 'bob');
+                    })
+                    .then((identity) => {
+                        bobIdentity = identity;
+                    });
+
+            });;
     });
 
     describe('#transactions', () => {
@@ -70,8 +154,8 @@ describe('e-krona', () => {
             return businessNetworkConnection.submitTransaction(tx).then(x => {
                 console.log('x', x)
                 //true.should.equal(false)
-            }).catch(y => console.log('y',y))
-            
+            }).catch(y => console.log('y', y))
+
         })
 
         /* it('should be able to trade a commodity', () => {
